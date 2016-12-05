e9ef55447d3b200e35e673231fe17e4385e1bd68
Shaverda <shaverda@gmail.com>
2016-12-05 13:24:34 -0600

    control flow, regex, log operators etc

--- app/count.js
+++ app/count.js
@@ -2,6 +2,21 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+  	var timeout;
+  	function count_it(){
+  		console.log(start++);
 
+  		if (start <= end) {
+  			timeout = setTimeout(count_it, 100);
+  		}
+  	}
+
+  	count_it();
+
+  	return {
+  		cancel: function () {
+  			timeout && clearTimeout(timeout);
+  		}
+  	}
   }
 };
--- app/flowControl.js
+++ app/flowControl.js
@@ -2,6 +2,38 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.flowControlAnswers = {
   fizzBuzz: function(num) {
+
+    function fizzbuzz (num){
+       if (typeof num !== 'number') {
+      return false;
+    }
+
+    // if the number is divisible by 3 AND 5, then when divided by both,
+    // the remainder for each operation will be zero
+    // return 'fizzbuzz'
+    if (num % 3 === 0 && num % 5 === 0) {
+      return 'fizzbuzz';
+    }
+
+    // if the number is divisible by 3, when divided by 3, the remainder is zero
+    // return 'fizz'
+    if (num % 3 === 0) {
+      return 'fizz';
+    }
+
+    // if the number is divisible by 5, when divided by 5, the remainder is zero
+    // return 'buzz'
+    if (num % 5 === 0) {
+      return 'buzz';
+    }
+
+    // if the number is not divisible by 3 or 5, i.e. has skipped all previous
+    // conditions, return the number
+    return num;
+
+        
+
+    }
     // write a function that receives a number as its argument;
     // if the number is divisible by 3, the function should return 'fizz';
     // if the number is divisible by 5, the function should return 'buzz';
